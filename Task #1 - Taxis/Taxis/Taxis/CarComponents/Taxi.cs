05858cb3f8e31f0185fb59eb27079f5c99f17ca6
Дмитрий <divo-voron@mail.ru>
2017-04-19 13:41:04 +0300

    New class

---
+++ Task #1 - Taxis/Taxis/Taxis/CarComponents/Taxi.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TaxiStation.Interfaces;
+
+namespace TaxiStation.CarComponents
+{
+    class Taxi : Car, ICollection<ICar>, IPassengers, ICargo
+    {
+        private ICollection<Car> _cars;
+
+        public int NumberOfPassengers
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+            set
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        public int Carrying
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+            set
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+
+
+        public void Add(ICar item)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Clear()
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Contains(ICar item)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void CopyTo(ICar[] array, int arrayIndex)
+        {
+            throw new NotImplementedException();
+        }
+
+        public int Count
+        {
+            get { throw new NotImplementedException(); }
+        }
+
+        public bool IsReadOnly
+        {
+            get { throw new NotImplementedException(); }
+        }
+
+        public bool Remove(ICar item)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerator<ICar> GetEnumerator()
+        {
+            throw new NotImplementedException();
+        }
+
+        System.Collections.IEnumerator System.Collections.IEnumerable.GetEnumerator()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
